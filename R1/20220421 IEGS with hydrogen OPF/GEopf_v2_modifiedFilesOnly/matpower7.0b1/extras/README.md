136d5fe865191179200fb9857206d31d4e8218dc
王盛ShengWang <wangsheng_zju@zju.edu.cn>
2023-10-27 16:27:57 +0100

    20231027

---
+++ R1/20220421 IEGS with hydrogen OPF/GEopf_v2_modifiedFilesOnly/matpower7.0b1/extras/README.md
@@ -0,0 +1,8 @@
+matpower-extras
+===============
+
+MATPOWER Extras - contributed and/or unsupported MATPOWER-related code
+
+To install, simply place the contents of this repository in a directory
+named `extras` inside the MATPOWER install directory `<MATPOWER>` and
+re-run `install_matpower`.
---
+++ R1/20220421 IEGS with hydrogen OPF/GEopf_v2_modifiedFilesOnly/matpower7.0b1/lib/Contents.m
@@ -0,0 +1,29 @@
+% MATPOWER
+%   Version 7.0b1       31-Oct-2018
+%
+%   MATPOWER is a package of M-files for solving power flow,
+%   continuation power flow and optimal power flow problems using
+%   MATLAB or Octave. It is intended as a simulation tool for
+%   researchers and educators that is easy to use and modify. MATPOWER
+%   is designed to give the best performance possible while keeping the
+%   code simple to understand and modify.
+%
+%   MATPOWER releases can be downloaded from the MATPOWER website:
+%
+%       http://www.pserc.cornell.edu/matpower/
+%
+%   and the latest stable and work-in-progress versions can always be
+%   downloaded or cloned from the MATPOWER GitHub project, whose
+%   `master` branch should always contain a stable version.
+%
+%       https://github.com/MATPOWER/matpower
+%
+%   MATPOWER is covered by the 3-clause BSD License (see LICENSE for details).
+
+%   MATPOWER
+%   Copyright (c) 2004-2018, Power Systems Engineering Research Center (PSERC)
+%   by Ray Zimmerman, PSERC Cornell
+%
+%   This file is part of MATPOWER.
+%   Covered by the 3-clause BSD License (see LICENSE file for details).
+%   See http://www.pserc.cornell.edu/matpower/ for more info.
